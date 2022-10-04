56555785ba9a12644e250559986c01216e3561d5
Carl Hinderer <carl.hinderer4@gmail.com>
2022-10-03 21:33:18 -0600

    Added Flask-Migrate.

--- customers_service/CustomersService/app/__init__.py
+++ customers_service/CustomersService/app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from config import config
 
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app(config_name):
@@ -14,6 +16,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     # We'll only run this in local Docker
     # app.app_context().push()
